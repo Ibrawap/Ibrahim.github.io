ac59085d32cfc8c9913192a1927ba7c1dbe5c644
Nicholas Calugar <njcalugar@gmail.com>
2012-01-20 18:18:43 -0800

    Updating classmap

--- module/Application/autoload_classmap.php
+++ module/Application/autoload_classmap.php
@@ -4,6 +4,7 @@ return array(
     'Application\Module'                          => __DIR__ . '/Module.php',
     'Application\Controller\ErrorController'      => __DIR__ . '/src/Application/Controller/ErrorController.php',
     'Application\Controller\IndexController'      => __DIR__ . '/src/Application/Controller/IndexController.php',
+    'Application\Controller\JobsController'       => __DIR__ . '/src/Application/Controller/JobsController.php',
     'Application\Mvc\Controller\ActionController' => __DIR__ . '/src/Application/Mvc/Controller/ActionController.php',
     'Application\View\Listener'                   => __DIR__ . '/src/Application/View/Listener.php',
 );
