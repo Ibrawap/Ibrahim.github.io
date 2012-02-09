415418e093aa50edc1aee6d89163049cde658246
Nicholas Calugar <njcalugar@gmail.com>
2012-02-09 12:08:25 -0800

    Trying to resolve APC warnings

--- public/index.php
+++ public/index.php
@@ -1,13 +1,16 @@
 <?php
 chdir(dirname(__DIR__));
-require_once (getenv('ZF2_PATH') ?: dirname(__DIR__) . '/vendor/ZendFramework/library') . '/Zend/Loader/AutoloaderFactory.php';
+$appDirectory = dirname(__DIR__);
+require_once (getenv('ZF2_PATH') ?: $appDirectory . '/vendor/ZendFramework/library') . '/Zend/Loader/AutoloaderFactory.php';
 Zend\Loader\AutoloaderFactory::factory(array('Zend\Loader\StandardAutoloader' => array()));
 
-$appConfig = include 'config/application.config.php';
+$appConfig = include $appDirectory . '/config/application.config.php';
 
 $listenerOptions  = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
 $defaultListeners = new Zend\Module\Listener\DefaultListenerAggregate($listenerOptions);
-$defaultListeners->getConfigListener()->addConfigGlobPath('config/autoload/*.config.php');
+$defaultListeners->getConfigListener()->addConfigGlobPath($appDirectory . '/config/autoload/*.config.php');
+
+unset($appDirectory);
 
 $moduleManager = new Zend\Module\Manager($appConfig['modules']);
 $moduleManager->events()->attachAggregate($defaultListeners);
