803f9871231290bda4bd806abc5a186d256148f7
Adam Passey <apassey@ign.com>
2012-03-06 17:16:08 -0800

    Added application overlay.

--- public/js/jquery-code-foo-controller.js
+++ public/js/jquery-code-foo-controller.js
@@ -1,9 +1,20 @@
+var Application = {
+	show : function() {
+		jQuery('.application-overlay').stop().animate({ top: 40 }, 500);
+	},
+	hide : function() {
+		jQuery('.application-overlay').stop().animate({ top: -1200 }, 500);
+	}
+};
+
 jQuery(document).ready(function() {
 	
 	jQuery('.cf-speakers .span2 a').hover(function() {
-		jQuery('span',this).stop().animate({ opacity: 1.0 },200);
+		jQuery('span',this).stop().animate({ opacity: 1.0 },100);
 	}, function() {
-		jQuery('span',this).stop().animate({ opacity: 0.0 },200);
+		jQuery('span',this).stop().animate({ opacity: 0.0 },100);
 	});;
+	
+	//Application.show();
 	
 });
